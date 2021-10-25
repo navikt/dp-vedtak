c11237d1a6494512a92f77e92aa791e25d1c0cf0
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2021-10-25 14:55:17 +0200

    glemte person
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

---
+++ modell/src/main/kotlin/no/nav/dagpenger/Person.kt
@@ -0,0 +1,19 @@
+package no.nav.dagpenger
+
+import no.nav.dagpenger.Hovedvedtak.Companion.erAktiv
+import no.nav.dagpenger.hendelse.ProsessResultatHendelse
+
+class Person private constructor(
+    private val vedtak: MutableList<Hovedvedtak>
+) {
+    constructor() : this(mutableListOf())
+
+    fun håndter(hendelse: ProsessResultatHendelse) {
+
+        if(vedtak.none {
+            it.håndter(hendelse)
+        }) { vedtak.add(hendelse.hovedvedtak)}
+    }
+
+    fun harDagpenger() = vedtak.any(::erAktiv)
+}
