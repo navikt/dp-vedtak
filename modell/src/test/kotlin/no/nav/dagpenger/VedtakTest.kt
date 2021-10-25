1653d93c52302cf04deea8948b9082b65e162a5a
André Roaldseth <andre.roaldseth@nav.no>
2021-10-25 14:06:01 +0200

    Drodling av ny modell
    Co-authored-by: Rannveig Alette Skjerve <rannveig.alette.skjerve@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Maria Cabrol <maria.cabrol@nav.no>

--- modell/src/test/kotlin/no/nav/dagpenger/VedtakTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/VedtakTest.kt
@@ -1,25 +1,70 @@
 package no.nav.dagpenger
 
-import no.nav.dagpenger.hendelse.SøknadOmNyRettighetHendelse
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 internal class VedtakTest {
+    private class Person private constructor(
+        private val vedtak: MutableList<Hovedvedtak>
+    ) {
+        constructor() : this(mutableListOf())
 
-    @Test
-    fun `Skal kunne opprettet sak`() {
-        val søknadshendelse = SøknadOmNyRettighetHendelse(søknadsReferanse = "referanse", fnr ="1234567890")
-        val person = Person()
-        person.søk(søknadshendelse)
-        assertTrue(person.harSak("referanse"))
+        fun håndter(hendelse: ManglendeMeldekortHendelse) {
+            vedtak.forEach {
+                it.håndter(hendelse)
+            }
+        }
+    }
+
+    private open class Vedtak(
+        private var omgjortAv: Vedtak?,
+    ) {
+        fun omgjøresAv(omgjøringsVedtak: Vedtak) {
+            omgjortAv = omgjøringsVedtak
+        }
+    }
+
+    private class Hovedvedtak private constructor(
+        private var tilstand: Tilstand,
+        private val endringer: MutableList<Vedtak>,
+        private val sats: Int,
+        omgjortAv: Vedtak?
+    ) : Vedtak(omgjortAv) {
+        constructor() : this(Tilstand.Aktiv, mutableListOf(), 0, null)
+
+        fun håndter(hendelse: ManglendeMeldekortHendelse) {
+            tilstand.håndter(this, hendelse)
+        }
+
+        private interface Tilstand {
+            fun håndter(hovedvedtak: Hovedvedtak, hendelse: ManglendeMeldekortHendelse)
+
+            object Aktiv : Tilstand
+            object Inaktiv : Tilstand
+            object Avsluttet : Tilstand
+        }
     }
 
     @Test
-    fun `En sak skal ikke ha duplikate søknader `() {
-        val søknadshendelse = SøknadOmNyRettighetHendelse(søknadsReferanse = "referanse", fnr ="1234567890")
+    fun `Har fått prosessresutalt fra quiz der alt er er oppfylt  innvilgelse`() {
         val person = Person()
-        person.søk(søknadshendelse)
-        person.søk(søknadshendelse)
-        assertTrue(person.harSak("referanse"))
+        // Ute i verden ett sted (mediator)
+        val hendelse = ProsessResultatHendelse(utfall = true)
+
+        person.håndter(hendelse)
+    }
+
+    @Test
+    fun `Har ikke sendt meldekort`() {
+        val person = Person()
+
+        // Ute i verden ett sted (mediator)
+        val hendelse = ManglendeMeldekortHendelse()
+
+        person.håndter(hendelse)
     }
 }
+
+interface Hendelse
+
+class ProsessResultatHendelse(val utfall: Boolean) : Hendelse
+class ManglendeMeldekortHendelse : Hendelse
