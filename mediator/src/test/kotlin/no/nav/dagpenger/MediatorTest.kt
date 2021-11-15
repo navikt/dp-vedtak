f0f10864e15eeb0eded6a5203e0531014dffaf29
Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
2021-11-15 12:53:20 +0100

    Laget feilende test for motta melding om prosessresultat
    navikt/dagpenger#984
    
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>
    Co-authored-by: rannveigskjerve <rannveig.alette.skjerve@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/MediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/MediatorTest.kt
@@ -1,10 +1,42 @@
-import org.junit.jupiter.api.Assertions.assertTrue
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
+import java.util.UUID
 
 internal class MediatorTest {
 
-    @Test
-    fun `hurra hurra`() {
-        assertTrue(false)
+    val testRapid = TestRapid()
+
+    @BeforeEach
+    fun setUp() {
+        testRapid.reset()
     }
+
+    @Test
+    fun `Skal kunne opprette avtale når vi mottar melding om prosessresultat`() {
+        testRapid.sendTestMessage(meldingOmInnvilget)
+        testRapid.inspektør.message(testRapid.inspektør.size - 1).also {
+            assertEquals("vedtak_opprettet", it["@event_name"].asText())
+        }
+    }
+
+    //language=JSON
+    private val meldingOmInnvilget = """
+        {
+        "@event_name": "prosess_resultat",
+        "@opprettet": ${LocalDate.now()},
+        "@id": ${UUID.randomUUID()},
+        "søknad_uuid": ${UUID.randomUUID()},
+        "resultat": true,
+        "identer": [{
+          "id": "123",
+          "type": "folkeregisterident",
+          "historisk": false
+        }],
+        "fakta": {},
+        "subsumsjoner": {}
+        }
+    """.trimIndent()
 }
