24108ea4d39935e2ca4fddf48ef3c8499179aaf7
André Roaldseth <andre.roaldseth@nav.no>
2021-11-17 19:53:19 +0100

    Legg til en TemporalCollection-klasse

---
+++ modell/src/main/kotlin/no/nav/dagpenger/vedtak/modell/TemporalCollection.kt
@@ -0,0 +1,17 @@
+package no.nav.dagpenger.vedtak.modell
+
+import java.time.LocalDate
+
+internal class TemporalCollection<R> {
+    private val contents = mutableMapOf<LocalDate, R>()
+    private val milestones get() = contents.keys.toList().reversed()
+
+    fun get(date: LocalDate): R = milestones
+        .firstOrNull { it.isBefore(date) || it.isEqual(date) }?.let {
+            contents[it]
+        } ?: throw IllegalArgumentException("No records that early")
+
+    fun put(at: LocalDate, item: R) {
+        contents[at] = item
+    }
+}
---
+++ modell/src/test/kotlin/no/nav/dagpenger/vedtak/modell/TemporalCollectionTest.kt
@@ -0,0 +1,34 @@
+package no.nav.dagpenger.vedtak.modell
+
+import no.nav.dagpenger.vedtak.modell.helpers.januar
+import no.nav.dagpenger.vedtak.modell.helpers.juli
+import no.nav.dagpenger.vedtak.modell.helpers.mars
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.math.BigDecimal
+
+internal class TemporalCollectionTest {
+    private lateinit var satser: TemporalCollection<BigDecimal>
+    private val lavSats = BigDecimal(5)
+    private val høySats = BigDecimal(10)
+
+    @BeforeEach
+    fun setUp() {
+        satser = TemporalCollection()
+        satser.put(1.mars, lavSats)
+        satser.put(4.juli, høySats)
+    }
+
+    @Test
+    fun `får riktig sats til riktig dato`() {
+        assertThrows<IllegalArgumentException> {
+            satser.get(1.januar)
+        }
+        assertEquals(lavSats, satser.get(1.mars))
+        assertEquals(lavSats, satser.get(1.juli))
+        assertEquals(høySats, satser.get(4.juli))
+        assertEquals(høySats, satser.get(15.juli))
+    }
+}
