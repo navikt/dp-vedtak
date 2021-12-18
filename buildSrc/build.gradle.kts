da137f9d7ab544f5d2f02db25dc6f64fe7e674ac
Giao The Cung <giaothe@gmail.com>
2021-12-18 13:05:37 +0100

    oppgradere log4j

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,17 +1,7 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.4.31"
 }
 
 repositories {
-    gradlePluginPortal()
-}
-
-dependencies {
-    implementation(kotlin("gradle-plugin"))
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:5.11.0")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+    jcenter()
 }
