1f0cbc666221c08b924e9290d6b943454bc4c271
Geir André Lund <geir.andre.lund@nav.no>
2021-12-21 13:37:23 +0100

    Oppdaterte ktlint til versjon 0.43.2

--- buildSrc/src/main/kotlin/dagpenger.spotless.gradle.kts
+++ buildSrc/src/main/kotlin/dagpenger.spotless.gradle.kts
@@ -6,11 +6,11 @@ plugins {
 
 spotless {
     kotlin {
-        ktlint("0.40.0")
+        ktlint(Ktlint.version)
     }
     kotlinGradle {
         target("*.gradle.kts")
-        ktlint("0.40.0")
+        ktlint(Ktlint.version)
     }
 }
 
