164c8e0f9d80a40ce18be81c2ec20012d03763de
Giao The Cung <giaothe@gmail.com>
2023-03-16 09:13:44 +0100

    bruke baseimages fra ghcr.io

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,17 +1,7 @@
 plugins {
     `kotlin-dsl`
-    kotlin("jvm") version "1.6.10"
 }
 
 repositories {
-    gradlePluginPortal()
-}
-
-dependencies {
-    implementation(kotlin("gradle-plugin"))
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:6.11.0")
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions.jvmTarget = JavaVersion.VERSION_17.toString()
+    mavenCentral()
 }
