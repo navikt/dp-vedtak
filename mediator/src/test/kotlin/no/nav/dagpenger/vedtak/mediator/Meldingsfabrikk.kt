bbac09034c07455e47a66b59bc4cdd6be9a45e94
Geir André Lund <geir.andre.lund@nav.no>
2023-04-12 11:20:17 +0200

    WIP - Meldingsformat søknad behandlet hendelse

--- mediator/src/test/kotlin/no/nav/dagpenger/vedtak/mediator/Meldingsfabrikk.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/vedtak/mediator/Meldingsfabrikk.kt
@@ -4,21 +4,27 @@ import java.time.LocalDate
 import java.util.UUID
 
 object Meldingsfabrikk {
-
     fun søknadInnvilgetJson() =
         //language=JSON
         """
         {        
-          "@event_name": "søknad_innvilget_hendelse",
+          "@event_name": "søknad_behandlet_hendelse",
+          "ident" : "12345123451",
           "behandlingId": "${UUID.randomUUID()}",
           "virkningsdato": "${LocalDate.now()}",
+          "innvilget": true
           "dagpengerettighet": "Ordinær",
           "dagsats": "500",
           "grunnlag": "500000",
           "stønadsperiode": "52",
           "vanligArbeidstidPerDag": "8",
           "antallVentedager": "3",
-          "ident" : "12345123451"
+          "barnetillegg" : [ {
+              "fødselsdato" : "2012-03-03"
+          }, {
+              "fødselsdato" : "2015-03-03"
+          }]
+           
         } 
         """.trimIndent()
 }
