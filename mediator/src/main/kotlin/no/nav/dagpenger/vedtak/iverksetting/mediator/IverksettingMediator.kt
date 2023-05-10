81e67302f44be4e50f2308f722157d00aa048332
Geir André Lund <geir.andre.lund@nav.no>
2023-05-10 12:56:14 +0200

    Todos
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/vedtak/iverksetting/mediator/IverksettingMediator.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/vedtak/iverksetting/mediator/IverksettingMediator.kt
@@ -2,6 +2,13 @@ package no.nav.dagpenger.vedtak.iverksetting.mediator
 
 import no.nav.dagpenger.vedtak.iverksetting.hendelser.VedtakFattetHendelse
 
+// @todo
+// 1. Hente eller opprette iverksetting
+// 2. Håndtere hendelse
+// 3. Lagre iverksetting
+// 4. Behovmediator - fra dp-soknad
+// 5. Lage behovløser for iversksetting mot iver
+
 internal class IverksettingMediator {
     fun behandle(vedtakFattetHendelseMessage: VedtakFattetHendelse) {
         TODO()
