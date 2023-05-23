f59ca680b45d1abe8d937cff4b5893376d46b3be
Marius Eriksen <marius.eriksen@nav.no>
2023-05-23 16:31:44 +0200

    Mediator bruker nå kun dependencies og versjoner fra libs.versions.toml
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- mediator/build.gradle.kts
+++ mediator/build.gradle.kts
@@ -6,26 +6,26 @@ plugins {
 dependencies {
     implementation(project(":modell"))
 
-    implementation(Jackson.core)
-    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:${libs.versions.jackson.get()}")
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.datatype.jsr310)
 
     // https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-slf4j/
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${libs.versions.kotlinx.coroutines.slf4j.get()}")
 
     // POC - iverksett api
-    implementation("io.ktor:ktor-client-core:${Ktor2.version}")
-    implementation("io.ktor:ktor-client-cio:${Ktor2.version}")
-    implementation("io.ktor:ktor-client-content-negotiation:${Ktor2.version}")
-    implementation("io.ktor:ktor-serialization-jackson:${Ktor2.version}")
-    implementation("io.ktor:ktor-client-logging:${Ktor2.version}")
-    implementation("com.github.navikt.dp-biblioteker:oauth2-klient:${Dagpenger.Biblioteker.version}")
+    implementation(libs.ktor.client.core)
+    implementation(libs.ktor.client.cio)
+    implementation(libs.ktor.client.content.negotiation)
+    implementation(libs.ktor.serialization.jackson)
+    implementation(libs.ktor.client.logging.jvm)
+    implementation("com.github.navikt.dp-biblioteker:oauth2-klient:${libs.versions.dagpenger.biblioteker.get()}")
 
     implementation(libs.rapids.and.rivers)
     implementation(libs.konfig)
     implementation(libs.kotlin.logging)
 
+    testImplementation(libs.ktor.client.mock)
     testImplementation("io.kotest:kotest-assertions-core-jvm:${libs.versions.kotest.get()}")
-    testImplementation("io.ktor:ktor-client-mock:${Ktor2.version}")
 
     testImplementation(Mockk.mockk)
 }
