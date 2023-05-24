712d6591631afd2aab6be2a66c78d368799ab130
Marius Eriksen <marius.eriksen@nav.no>
2023-05-24 12:47:08 +0200

    refaktorer compareTo slik at den blir mer kotlinifisert
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>

--- modell/src/main/kotlin/no/nav/dagpenger/vedtak/modell/mengde/RatioMengde.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/vedtak/modell/mengde/RatioMengde.kt
@@ -1,7 +1,9 @@
 package no.nav.dagpenger.vedtak.modell.mengde
 
 // Forstår ulike mengder hvor det finnes et absolutt nullpunkt, som f.eks. alder, penger, tid
-open class RatioMengde internal constructor(private val mengde: Number, enhet: Enhet) : IntervallMengde(mengde, enhet), Comparable<RatioMengde> {
+open class RatioMengde internal constructor(private val mengde: Number, enhet: Enhet) :
+    IntervallMengde(mengde, enhet),
+    Comparable<RatioMengde> {
     operator fun unaryPlus() = this
 
     operator fun unaryMinus() = RatioMengde(-amount, enhet)
@@ -9,18 +11,13 @@ open class RatioMengde internal constructor(private val mengde: Number, enhet: E
     operator fun plus(other: RatioMengde) = RatioMengde(this.amount + convertedAmount(other), enhet)
 
     operator fun minus(other: RatioMengde) = this + -other
+
     override fun compareTo(other: RatioMengde): Int {
-        if (!this.enhet.isCompatible(other.enhet)) {
-            throw IllegalArgumentException("Kan bare sammenligne med samme enhet.")
-        }
-        return if (this.mengde.toDouble() == other.mengde.toDouble()) {
-            0
-        } else if (this.mengde.toDouble() < other.mengde.toDouble()) {
-            -1
-        } else {
-            1
+        require(this.enhet.isCompatible(other.enhet)) { "Kan bare sammenligne med samme enhet." }
+        return when {
+            this.mengde.toDouble() == other.mengde.toDouble() -> 0
+            this.mengde.toDouble() < other.mengde.toDouble() -> -1
+            else -> 1
         }
     }
-
-    // fun erKompatibel(postering: List<Postering>) = postering.first().mengde.enhet.isCompatible(this.enhet)
 }
