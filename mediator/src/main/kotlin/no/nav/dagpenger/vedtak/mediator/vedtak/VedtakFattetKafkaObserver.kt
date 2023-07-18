1f3f6b29b8a677c8bed98c44fe60c1de2dc722e5
Geir André Lund <geir.andre.lund@nav.no>
2023-07-18 13:05:53 +0200

    Logger med behandlingId og vedtakId i logging-kontekst.

--- mediator/src/main/kotlin/no/nav/dagpenger/vedtak/mediator/vedtak/VedtakFattetKafkaObserver.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/vedtak/mediator/vedtak/VedtakFattetKafkaObserver.kt
@@ -1,12 +1,11 @@
 package no.nav.dagpenger.vedtak.mediator.vedtak
 
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.vedtak.modell.PersonObserver
 import no.nav.dagpenger.vedtak.modell.vedtak.VedtakObserver
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
-
-val behandlingId = "behandlingId"
 
 internal class VedtakFattetKafkaObserver(private val rapidsConnection: RapidsConnection) : PersonObserver {
 
@@ -16,46 +15,60 @@ internal class VedtakFattetKafkaObserver(private val rapidsConnection: RapidsCon
     }
 
     override fun vedtakFattet(ident: String, vedtakFattet: VedtakObserver.VedtakFattet) {
-        sikkerlogger.info { "Vedtak for $ident fattet. Vedtak: $vedtakFattet" }
-        val message = JsonMessage.newMessage(
-            eventName = "vedtak_fattet",
-            map = mapOf(
-                "ident" to ident,
+        withLoggingContext(
+            mapOf(
                 "behandlingId" to vedtakFattet.behandlingId.toString(),
                 "vedtakId" to vedtakFattet.vedtakId.toString(),
-                "vedtaktidspunkt" to vedtakFattet.vedtakstidspunkt,
-                "virkningsdato" to vedtakFattet.virkningsdato,
-                "utfall" to vedtakFattet.utfall.name,
             ),
-        )
+        ) {
+            sikkerlogger.info { "Vedtak for $ident fattet. Vedtak: $vedtakFattet" }
+            val message = JsonMessage.newMessage(
+                eventName = "vedtak_fattet",
+                map = mapOf(
+                    "ident" to ident,
+                    "behandlingId" to vedtakFattet.behandlingId.toString(),
+                    "vedtakId" to vedtakFattet.vedtakId.toString(),
+                    "vedtaktidspunkt" to vedtakFattet.vedtakstidspunkt,
+                    "virkningsdato" to vedtakFattet.virkningsdato,
+                    "utfall" to vedtakFattet.utfall.name,
+                ),
+            )
 
-        rapidsConnection.publish(
-            key = ident,
-            message = message.toJson(),
-        )
-        logger.info { "Vedtak fattet melding publisert. BehandlingId: $behandlingId" }
+            rapidsConnection.publish(
+                key = ident,
+                message = message.toJson(),
+            )
+            logger.info { "Vedtak fattet melding publisert." }
+        }
     }
 
     override fun løpendeVedtakFattet(ident: String, løpendeVedtakFattet: VedtakObserver.LøpendeVedtakFattet) {
-        sikkerlogger.info { "Vedtak for $ident fattet. Vedtak: $løpendeVedtakFattet" }
-
-        val message = JsonMessage.newMessage(
-            eventName = "vedtak_fattet",
-            map = mapOf(
-                "ident" to ident,
+        withLoggingContext(
+            mapOf(
                 "behandlingId" to løpendeVedtakFattet.behandlingId.toString(),
                 "vedtakId" to løpendeVedtakFattet.vedtakId.toString(),
-                "vedtaktidspunkt" to løpendeVedtakFattet.vedtakstidspunkt,
-                "virkningsdato" to løpendeVedtakFattet.virkningsdato,
-                "utbetalingsdager" to løpendeVedtakFattet.utbetalingsdager,
-                "utfall" to løpendeVedtakFattet.utfall.name,
             ),
-        )
+        ) {
+            sikkerlogger.info { "Vedtak for $ident fattet. Vedtak: $løpendeVedtakFattet" }
 
-        rapidsConnection.publish(
-            key = ident,
-            message = message.toJson(),
-        )
-        logger.info { "Vedtak fattet melding publisert. BehandlingId: $behandlingId" }
+            val message = JsonMessage.newMessage(
+                eventName = "vedtak_fattet",
+                map = mapOf(
+                    "ident" to ident,
+                    "behandlingId" to løpendeVedtakFattet.behandlingId.toString(),
+                    "vedtakId" to løpendeVedtakFattet.vedtakId.toString(),
+                    "vedtaktidspunkt" to løpendeVedtakFattet.vedtakstidspunkt,
+                    "virkningsdato" to løpendeVedtakFattet.virkningsdato,
+                    "utbetalingsdager" to løpendeVedtakFattet.utbetalingsdager,
+                    "utfall" to løpendeVedtakFattet.utfall.name,
+                ),
+            )
+
+            rapidsConnection.publish(
+                key = ident,
+                message = message.toJson(),
+            )
+            logger.info { "Vedtak fattet melding publisert." }
+        }
     }
 }
