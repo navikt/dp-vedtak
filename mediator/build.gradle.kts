010ed087d0d3bb1aa465c710a4961f0549c5b24e
Geir André Lund <geir.andre.lund@nav.no>
2023-07-19 15:09:25 +0200

    Fjernet avhengigher til dp-kontrakter - flyttet til egen behovløser -> dp-behov-iverksett.

--- mediator/build.gradle.kts
+++ mediator/build.gradle.kts
@@ -3,39 +3,15 @@ plugins {
     application
 }
 
-val githubUser: String? by project
-val githubPassword: String? by project
-
-repositories {
-    maven {
-        url = uri("https://maven.pkg.github.com/navikt/dp-kontrakter")
-        credentials {
-            username = githubUser
-            password = githubPassword
-        }
-    }
-}
-
 dependencies {
     implementation(project(path = ":modell"))
     implementation(project(path = ":aktivitetslogg"))
-
-    // Kontrakter for dp-iverksett
-    implementation("no.nav.dagpenger.kontrakter:iverksett:2.0_20230620164418_dc7edb7")
 
     implementation(libs.jackson.core)
     implementation(libs.jackson.datatype.jsr310)
 
     // https://kotlinlang.org/api/kotlinx.coroutines/kotlinx-coroutines-slf4j/
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j:${libs.versions.kotlinx.coroutines.slf4j.get()}")
-
-    // POC - iverksett api
-    implementation(libs.ktor.client.core)
-    implementation(libs.ktor.client.cio)
-    implementation(libs.ktor.client.content.negotiation)
-    implementation(libs.ktor.serialization.jackson)
-    implementation(libs.ktor.client.logging.jvm)
-    implementation("com.github.navikt.dp-biblioteker:oauth2-klient:${libs.versions.dagpenger.biblioteker.get()}")
 
     implementation(libs.rapids.and.rivers)
     implementation(libs.konfig)
