9cd009c91be5662387c8c6eabfdfd0aa86c173a3
Abdifatah Bashi <abdifatah.bashi@nav.no>
2023-07-24 13:42:37 +0200

    Hente person fra database og hindrer feil ved duplikat lagriing.
    Co-authored-by: Mona Kjeldsrud <mona.kjeldsrud@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/vedtak/mediator/persistens/PostgresPersonRepository.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/vedtak/mediator/persistens/PostgresPersonRepository.kt
@@ -7,12 +7,27 @@ import kotliquery.sessionOf
 import kotliquery.using
 import no.nav.dagpenger.vedtak.modell.Person
 import no.nav.dagpenger.vedtak.modell.PersonIdentifikator
+import no.nav.dagpenger.vedtak.modell.PersonIdentifikator.Companion.tilPersonIdentfikator
 import no.nav.dagpenger.vedtak.modell.visitor.PersonVisitor
 import javax.sql.DataSource
 
 class PostgresPersonRepository(private val dataSource: DataSource) : PersonRepository {
     override fun hent(ident: PersonIdentifikator): Person? {
-        TODO("Not yet implemented")
+        return using(sessionOf(dataSource)) { session ->
+            session.run(
+                queryOf(
+                    //language=PostgreSQL
+                    statement = """
+                     SELECT * FROM person WHERE ident = :ident
+                    """.trimIndent(),
+                    paramMap = mapOf("ident" to ident.identifikator()),
+                ).map { row ->
+                    Person(
+                        row.string("ident").tilPersonIdentfikator(),
+                    )
+                }.asSingle,
+            )
+        }
     }
 
     override fun lagre(person: Person) {
@@ -41,7 +56,7 @@ class LagrePersonVisitor(person: Person) : PersonVisitor {
         queries.add(
             queryOf(
                 //language=PostgreSQL
-                statement = """INSERT INTO person (ident) VALUES (:ident)""",
+                statement = """INSERT INTO person (ident) VALUES (:ident) ON CONFLICT DO NOTHING""",
                 paramMap = mapOf("ident" to personIdentifikator.identifikator()),
             ),
         )
--- mediator/src/test/kotlin/no/nav/dagpenger/vedtak/mediator/persistens/PostgresPersonRepositoryTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/vedtak/mediator/persistens/PostgresPersonRepositoryTest.kt
@@ -5,16 +5,24 @@ import no.nav.dagpenger.vedtak.db.PostgresDataSourceBuilder
 import no.nav.dagpenger.vedtak.modell.Person
 import no.nav.dagpenger.vedtak.modell.PersonIdentifikator
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import kotlin.test.assertEquals
 
 class PostgresPersonRepositoryTest {
 
     @Test
     fun `lagre og hent person`() {
-        val person = Person(ident = PersonIdentifikator("12345678901"))
+        val ident = PersonIdentifikator("12345678901")
+        val person = Person(ident = ident)
 
         withMigratedDb {
             val postgresPersonRepository = PostgresPersonRepository(PostgresDataSourceBuilder.dataSource)
             postgresPersonRepository.lagre(person)
+            assertDoesNotThrow {
+                postgresPersonRepository.lagre(person)
+            }
+            val hentetPerson = postgresPersonRepository.hent(ident)
+            assertEquals(person.ident(), hentetPerson?.ident())
         }
     }
 }
