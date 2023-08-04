bfe8d594cf955af52c8d18c54258a9c949a57657
Marius Eriksen <marius.eriksen@nav.no>
2023-08-04 12:45:13 +0200

    wip: Hyssing og binders for å få start på openapi

---
+++ openapi/build.gradle.kts
@@ -0,0 +1,51 @@
+plugins {
+    id("org.openapi.generator") version "6.6.0"
+    id("common")
+    `java-library`
+}
+
+tasks.named("compileKotlin").configure {
+    dependsOn("openApiGenerate")
+}
+
+tasks.named("spotlessKotlin").configure {
+    dependsOn("openApiGenerate")
+}
+
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("src/main/kotlin", "$buildDir/generated/src/main/kotlin"))
+        }
+    }
+}
+
+spotless {
+    kotlin {
+        targetExclude("**/generated/**")
+    }
+}
+
+dependencies {
+    implementation(libs.jackson.annotation)
+}
+
+openApiGenerate {
+    generatorName.set("kotlin-server")
+    inputSpec.set("$projectDir/src/main/resources/vedtak-api.yaml")
+    outputDir.set("$buildDir/generated/")
+    packageName.set("no.nav.dagpenger.vedtak.api")
+    globalProperties.set(
+        mapOf(
+            "apis" to "none",
+            "models" to "",
+        ),
+    )
+    modelNameSuffix.set("DTO")
+    configOptions.set(
+        mapOf(
+            "serializationLibrary" to "jackson",
+            "enumPropertyNaming" to "original",
+        ),
+    )
+}
