3261df3557bb5626b509c6bd354efc4b48bcd3f3
Geir André Lund <geir.andre.lund@nav.no>
2023-08-25 13:52:53 +0200

    Degradere org.openapi.generator til 6.6.0 igjen

--- openapi/build.gradle.kts
+++ openapi/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.openapi.generator") version "7.0.0"
+    id("org.openapi.generator") version "6.6.0"
     id("common")
     `java-library`
 }
@@ -15,7 +15,7 @@ tasks.named("spotlessKotlin").configure {
 sourceSets {
     main {
         java {
-            setSrcDirs(listOf("src/main/kotlin", "${layout.buildDirectory}/generated/src/main/kotlin"))
+            setSrcDirs(listOf("src/main/kotlin", "${layout.buildDirectory.get()}/generated/src/main/kotlin"))
         }
     }
 }
@@ -33,7 +33,7 @@ dependencies {
 openApiGenerate {
     generatorName.set("kotlin-server")
     inputSpec.set("$projectDir/src/main/resources/vedtak-api.yaml")
-    outputDir.set("${layout.buildDirectory}/generated/")
+    outputDir.set("${layout.buildDirectory.get()}/generated/")
     packageName.set("no.nav.dagpenger.vedtak.api")
     globalProperties.set(
         mapOf(
