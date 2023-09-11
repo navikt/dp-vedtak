953e0aadcf60aa20186dbeca4a0b0e5517b5b7c1
Geir André Lund <geir.andre.lund@nav.no>
2023-09-11 11:19:18 +0200

    tilgang til å endre basen

--- mediator/src/main/resources/db/migration/R__USER_PRIVILIEGES.sql
+++ mediator/src/main/resources/db/migration/R__USER_PRIVILIEGES.sql
@@ -6,7 +6,7 @@ $$
             (SELECT 1 FROM pg_roles WHERE rolname = 'cloudsqliamuser')
         THEN
             REVOKE ALL ON ALL TABLES IN SCHEMA public FROM cloudsqliamuser;
-            GRANT SELECT ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
+            GRANT ALL ON ALL TABLES IN SCHEMA public TO cloudsqliamuser;
         END IF;
     END
 $$;
