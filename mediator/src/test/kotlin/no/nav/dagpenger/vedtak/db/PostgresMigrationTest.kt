d189bbc65524241abe3c0dea19384c5a1fa5a848
Mona Kjeldsrud <mona.kjeldsrud@nav.no>
2023-10-02 13:41:30 +0200

    Fjernet iverkstting-tabell, siden den ikke lenger er i bruk
    Co-authored-by: Marius Eriksen <marius.eriksen@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/vedtak/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/vedtak/db/PostgresMigrationTest.kt
@@ -10,7 +10,7 @@ class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            Assertions.assertEquals(5, migrations)
+            Assertions.assertEquals(6, migrations)
         }
     }
 }
