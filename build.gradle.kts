e762eb6be76af3598d26de91ac38d559442d875e
Geir André Lund <geir.andre.lund@nav.no>
2024-01-29 19:00:58 +0100

    La til gradle fil for opplysninger også.

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+plugins {
+    id("common")
+    `java-library`
+}
+val cucumberVersion = "7.15.0"
+dependencies {
+    testImplementation("io.cucumber:cucumber-java8:$cucumberVersion")
+    testImplementation("io.cucumber:cucumber-junit-platform-engine:$cucumberVersion")
+    testImplementation("org.junit.platform:junit-platform-suite:1.10.1")
+}
