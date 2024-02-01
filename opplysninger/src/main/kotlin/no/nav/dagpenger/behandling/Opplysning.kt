dde5c267bf02afac1df9d34b1f23fb88281e920c
André Roaldseth <andre.roaldseth@nav.no>
2024-02-01 13:58:06 +0100

    Fjern ubrukte constructors i Opplysning

--- opplysninger/src/main/kotlin/no/nav/dagpenger/behandling/Opplysning.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/behandling/Opplysning.kt
@@ -1,7 +1,6 @@
 package no.nav.dagpenger.behandling
 
 import no.nav.dagpenger.behandling.regel.Regel
-import java.time.LocalDateTime
 import java.util.UUID
 
 data class Utledning(
@@ -22,7 +21,8 @@ sealed class Opplysning<T : Comparable<T>>(
 ) : Klassifiserbart by opplysningstype {
     abstract fun bekreft(): Faktum<T>
 
-    override fun toString() = "${javaClass.simpleName} om ${opplysningstype.navn} har verdi: $verdi som er $gyldighetsperiode"
+    override fun toString() =
+        "${javaClass.simpleName} om ${opplysningstype.navn} har verdi: $verdi som er $gyldighetsperiode"
 
     fun sammeSom(opplysning: Opplysning<*>): Boolean {
         return opplysningstype == opplysning.opplysningstype && gyldighetsperiode.overlapp(opplysning.gyldighetsperiode)
@@ -36,11 +36,6 @@ class Hypotese<T : Comparable<T>>(
     utledetAv: Utledning? = null,
     kilde: Kilde? = null,
 ) : Opplysning<T>(opplysningstype, verdi, gyldighetsperiode, utledetAv, kilde) {
-    constructor(opplysningstype: Opplysningstype<T>, verdi: T, gyldighetsperiode: ClosedRange<LocalDateTime>) : this(
-        opplysningstype,
-        verdi,
-        Gyldighetsperiode(gyldighetsperiode.start, gyldighetsperiode.endInclusive),
-    )
 
     override fun bekreft() = Faktum(super.opplysningstype, verdi, gyldighetsperiode, utledetAv)
 }
@@ -52,11 +47,6 @@ class Faktum<T : Comparable<T>>(
     utledetAv: Utledning? = null,
     kilde: Kilde? = null,
 ) : Opplysning<T>(opplysningstype, verdi, gyldighetsperiode, utledetAv, kilde) {
-    constructor(opplysningstype: Opplysningstype<T>, verdi: T, gyldighetsperiode: ClosedRange<LocalDateTime>) : this(
-        opplysningstype,
-        verdi,
-        Gyldighetsperiode(gyldighetsperiode.start, gyldighetsperiode.endInclusive),
-    )
 
     override fun bekreft() = this
 }
