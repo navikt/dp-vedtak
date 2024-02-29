0f760825756195c894e4dcc7b1be3d83085aedd9
André Roaldseth <andre.roaldseth@nav.no>
2024-02-29 15:24:37 +0100

    Lag en TestCase som lager sammenhengende testdata

--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/PersonMediatorTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/PersonMediatorTest.kt
@@ -1,6 +1,8 @@
 package no.nav.dagpenger.behandling
 
-import io.kotest.matchers.collections.shouldContainOnly
+import com.fasterxml.jackson.databind.JsonNode
+import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldContainAll
 import io.kotest.matchers.shouldBe
 import io.mockk.mockk
 import no.nav.dagpenger.behandling.db.InMemoryMeldingRepository
@@ -8,7 +10,6 @@ import no.nav.dagpenger.behandling.db.InMemoryPersonRepository
 import no.nav.dagpenger.behandling.mediator.BehovMediator
 import no.nav.dagpenger.behandling.mediator.HendelseMediator
 import no.nav.dagpenger.behandling.mediator.PersonMediator
-import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.asLocalDate
 import no.nav.helse.rapids_rivers.testsupport.TestRapid
 import org.junit.jupiter.api.BeforeEach
@@ -16,19 +17,19 @@ import org.junit.jupiter.api.Test
 import java.time.LocalDate
 
 internal class PersonMediatorTest {
-    private val testRapid = TestRapid()
+    private val rapid = TestRapid()
     private val ident = "11109233444"
 
     private val personRepository = InMemoryPersonRepository()
 
     init {
         HendelseMediator(
-            rapidsConnection = testRapid,
+            rapidsConnection = rapid,
             personMediator =
                 PersonMediator(
                     personRepository = personRepository,
                     aktivitetsloggMediator = mockk(relaxed = true),
-                    behovMediator = BehovMediator(testRapid),
+                    behovMediator = BehovMediator(rapid),
                 ),
             hendelseRepository = InMemoryMeldingRepository(),
         )
@@ -36,116 +37,47 @@ internal class PersonMediatorTest {
 
     @BeforeEach
     fun setUp() {
-        testRapid.reset()
+        rapid.reset()
         personRepository.reset()
     }
 
     @Test
     fun `e2e av søknad innsendt`() {
-        testRapid.sendTestMessage(søknadInnsendtMessage(ident))
+        val testCase = TestCase(ident, rapid)
+        testCase.sendSøknad()
 
-        with(testRapid.inspektør) {
-            size shouldBe 1
-            field(0, "@event_name").asText() shouldBe "behov"
-            field(0, "@behov").map { it.asText() }.shouldContainOnly(
-                "Fødselsdato",
-                "Søknadstidspunkt",
-                "ØnskerDagpengerFraDato",
-            )
+        rapid.harBehov("Fødselsdato", "Søknadstidspunkt", "ØnskerDagpengerFraDato")
+        testCase.løsOpplysningsbehov("Fødseldato", "Søknadstidspunkt", "ØnskerDagpengerFraDato")
+
+        rapid.harBehov("InntektId") {
+            dato("Siste avsluttende kalendermåned") shouldBe LocalDate.of(2021, 4, 30)
+            dato("Opptjeningsperiode") shouldBe LocalDate.of(2018, 4, 1)
         }
-        testRapid.sendTestMessage(opplysningSvar1Message(ident))
+        testCase.løsOpplysningsbehov("InntektId")
 
-        with(testRapid.inspektør) {
-            size shouldBe 2
-            field(1, "@behov").map { it.asText() }.shouldContainOnly(
-                "InntektId",
-            )
-            with(field(1, "InntektId")) {
-                this.size() shouldBe 3
-                get("Siste avsluttende kalendermåned").asLocalDate() shouldBe LocalDate.of(2021, 4, 30)
-                get("Opptjeningsperiode").asLocalDate() shouldBe LocalDate.of(2018, 4, 1)
-            }
-        }
+        rapid.harBehov("InntektSiste12Mnd") { tekst("InntektId") shouldBe testCase.inntektId }
+        rapid.harBehov("InntektSiste36Mnd") { tekst("InntektId") shouldBe testCase.inntektId }
 
-        testRapid.sendTestMessage(opplysningSvar2Message(ident))
-        with(testRapid.inspektør) {
-            size shouldBe 3
-            field(2, "@behov").map { it.asText() }.shouldContainOnly(
-                "InntektSiste12Mnd",
-                "InntektSiste36Mnd",
-            )
-            with(field(2, "InntektSiste12Mnd")) {
-                this.size() shouldBe 2
-                get("InntektId").asText() shouldBe inntektId
-            }
-            with(field(2, "InntektSiste36Mnd")) {
-                this.size() shouldBe 2
-                get("InntektId").asText() shouldBe inntektId
-            }
-        }
-
-        testRapid.sendTestMessage(opplysningSvar3Message(ident))
+        testCase.løsOpplysningsbehov("InntektSiste12Mnd", "InntektSiste36Mnd")
     }
+}
 
-    private fun søknadInnsendtMessage(ident: String) =
-        JsonMessage.newMessage(
-            "innsending_ferdigstilt",
-            mapOf(
-                "type" to "NySøknad",
-                "fødselsnummer" to ident,
-                "søknadsData" to
-                    mapOf(
-                        "søknad_uuid" to "e2e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e",
-                    ),
-            ),
-        ).toJson()
+private fun TestRapid.harBehov(vararg behov: String) {
+    withClue("Siste melding på rapiden skal inneholde behov: ${behov.toList()}") {
+        inspektør.message(inspektør.size - 1)["@behov"].map { it.asText() } shouldContainAll behov.toList()
+    }
+}
 
-    private val søknadstidspunkt = LocalDate.of(2021, 5, 5)
+private fun TestRapid.harBehov(
+    behov: String,
+    block: BehovHelper.() -> Unit,
+) {
+    harBehov(behov)
+    BehovHelper(inspektør.message(inspektør.size - 1)[behov]).apply { block() }
+}
 
-    private fun opplysningSvar1Message(ident: String) =
-        JsonMessage.newMessage(
-            "behov",
-            mapOf(
-                "ident" to ident,
-                "behandlingId" to "e2e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e",
-                "@final" to true,
-                "@løsning" to
-                    mapOf(
-                        "Fødselsdato" to LocalDate.of(1990, 1, 1),
-                        "Søknadstidspunkt" to søknadstidspunkt,
-                        "ØnskerDagpengerFraDato" to søknadstidspunkt,
-                    ),
-            ),
-        ).toJson()
+private class BehovHelper(private val message: JsonNode) {
+    fun tekst(navn: String) = message.get(navn)?.asText()
 
-    private val inntektId = "01HQTE3GBWCSVYH6S436DYFREN"
-
-    private fun opplysningSvar2Message(ident: String) =
-        JsonMessage.newMessage(
-            "behov",
-            mapOf(
-                "ident" to ident,
-                "behandlingId" to "e2e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e",
-                "@final" to true,
-                "@løsning" to
-                    mapOf(
-                        "InntektId" to inntektId,
-                    ),
-            ),
-        ).toJson()
-
-    private fun opplysningSvar3Message(ident: String) =
-        JsonMessage.newMessage(
-            "behov",
-            mapOf(
-                "ident" to ident,
-                "behandlingId" to "e2e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e",
-                "@final" to true,
-                "@løsning" to
-                    mapOf(
-                        "InntektSiste12Mnd" to 1234,
-                        "InntektSiste36Mnd" to 1234,
-                    ),
-            ),
-        ).toJson()
+    fun dato(navn: String) = message.get(navn)?.asLocalDate()
 }
---
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/TestCase.kt
@@ -0,0 +1,57 @@
+package no.nav.dagpenger.behandling
+
+import no.nav.helse.rapids_rivers.JsonMessage
+import no.nav.helse.rapids_rivers.testsupport.TestRapid
+import java.time.LocalDate
+
+class TestCase(
+    private val ident: String,
+    private val rapid: TestRapid,
+    private val søknadstidspunkt: LocalDate = LocalDate.of(2021, 5, 5),
+    private val alder: Int = 30,
+) {
+    val inntektId = "01HQTE3GBWCSVYH6S436DYFREN"
+    val søknadId = "e2e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e"
+    val behandlingId = "e3e9e3e0-8e3e-4e3e-8e3e-0e3e8e3e9e3e"
+
+    fun sendSøknad() = rapid.sendTestMessage(søknadInnsendt())
+
+    private fun søknadInnsendt() =
+        JsonMessage.newMessage(
+            "innsending_ferdigstilt",
+            mapOf(
+                "type" to "NySøknad",
+                "fødselsnummer" to ident,
+                "søknadsData" to
+                    mapOf(
+                        "søknad_uuid" to søknadId,
+                    ),
+            ),
+        ).toJson()
+
+    fun løsOpplysningsbehov(vararg behov: String) {
+        val behovSomLøses = løsninger.filterKeys { it in behov }
+        rapid.sendTestMessage(løstOpplysningsbehov(behovSomLøses))
+    }
+
+    private fun løstOpplysningsbehov(løsninger: Map<String, Any>) =
+        JsonMessage.newMessage(
+            "behov",
+            mapOf(
+                "ident" to ident,
+                "behandlingId" to behandlingId,
+                "@final" to true,
+                "@løsning" to løsninger,
+            ),
+        ).toJson()
+
+    private val løsninger =
+        mapOf(
+            "Fødselsdato" to søknadstidspunkt.minusYears(alder.toLong()),
+            "Søknadstidspunkt" to søknadstidspunkt,
+            "ØnskerDagpengerFraDato" to søknadstidspunkt,
+            "InntektId" to inntektId,
+            "InntektSiste12Mnd" to 1234,
+            "InntektSiste36Mnd" to 1234,
+        )
+}
