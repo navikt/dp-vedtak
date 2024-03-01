ff25ce8056a1d89febe1d674222b78e35b742d15
Geir André Lund <geir.andre.lund@nav.no>
2024-03-01 15:13:31 +0100

    Tydeliggjøre

--- dagpenger/src/main/kotlin/no/nav/dagpenger/regel/dato/SisteAvsluttendeKalenderMåned.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/regel/dato/SisteAvsluttendeKalenderMåned.kt
@@ -11,21 +11,22 @@ class SisteAvsluttendeKalenderMåned internal constructor(
     private val rapporteringsfrist: Opplysningstype<LocalDate>,
 ) : Regel<LocalDate>(produserer, listOf(søknadstidspunkt, rapporteringsfrist)) {
     override fun kjør(opplysninger: LesbarOpplysninger): LocalDate {
-        val a = opplysninger.finnOpplysning(søknadstidspunkt).verdi
-        val b = opplysninger.finnOpplysning(rapporteringsfrist).verdi
+        val søknadsdato = opplysninger.finnOpplysning(søknadstidspunkt).verdi
+        val rapporteringsdato = opplysninger.finnOpplysning(rapporteringsfrist).verdi
         val månederTilbake =
-            if (a.isBefore(b) || a.isEqual(b)) {
-                2
-            } else {
-                1
+            when (søknadsdato.førEllerLik(rapporteringsdato)) {
+                true -> 2
+                false -> 1
             }
 
-        val avsluttendeKalenderMåned = b.minusMonths(månederTilbake.toLong())
+        val avsluttendeKalenderMåned = rapporteringsdato.minusMonths(månederTilbake.toLong())
         return avsluttendeKalenderMåned.withDayOfMonth(avsluttendeKalenderMåned.lengthOfMonth())
     }
 
     override fun toString() = "Siste avsluttende kalendermåned for $søknadstidspunkt og $rapporteringsfrist"
 }
+
+private fun LocalDate.førEllerLik(dato: LocalDate) = this.isBefore(dato) || this.isEqual(dato)
 
 fun Opplysningstype<LocalDate>.sisteAvsluttendeKalenderMåned(
     søknadstidspunkt: Opplysningstype<LocalDate>,
