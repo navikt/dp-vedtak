51c04a2a69d59c16719dcd5b335d03764acc8be4
André Roaldseth <andre.roaldseth@nav.no>
2024-03-04 17:48:18 +0100

    Lag en test som rekursivt lager behandlinger i en kjede

--- modell/src/test/kotlin/no/nav/dagpenger/behandling/modell/BehandlingTest.kt
+++ modell/src/test/kotlin/no/nav/dagpenger/behandling/modell/BehandlingTest.kt
@@ -1,9 +1,11 @@
 package no.nav.dagpenger.behandling.modell
 
+import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
 import no.nav.dagpenger.behandling.modell.hendelser.SøknadInnsendtHendelse
 import no.nav.dagpenger.opplysning.Faktum
+import no.nav.dagpenger.opplysning.Gyldighetsperiode
 import no.nav.dagpenger.opplysning.Opplysningstype
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
@@ -18,31 +20,36 @@ internal class BehandlingTest {
             meldingsreferanseId = søknadId,
             gjelderDato = LocalDate.now(),
         )
-    val tidligerOpplysning = Opplysningstype.somDesimaltall("opplysning-fra-tidligere-behandling")
-    private val tidligerBehandling =
-        Behandling(
-            behandler = søknadInnsendtHendelse,
-            opplysninger =
-                listOf(
-                    Faktum(
-                        tidligerOpplysning,
-                        0.5,
-                    ),
-                ),
-        )
-
     private val testObservatør = TestObservatør()
+    private val tidligereOpplysning = Opplysningstype.somDesimaltall("opplysning-fra-tidligere-behandling")
 
     @Test
     fun `Behandling basert på tidligere behandlinger`() {
-        val behandling =
-            Behandling(
-                behandler = søknadInnsendtHendelse,
-                opplysninger = emptyList(),
-                basertPå = listOf(tidligerBehandling),
-            )
+        val behandlingskjede = behandlingskjede(5, søknadInnsendtHendelse)
+        behandlingskjede.opplysninger().finnAlle() shouldHaveSize 5
+        behandlingskjede.opplysninger().finnAlle().map {
+            it.verdi
+        } shouldBe listOf(1.0, 2.0, 3.0, 4.0, 5.0)
+    }
 
-        behandling.opplysninger().har(tidligerOpplysning) shouldBe true
+    private fun behandlingskjede(
+        antall: Int,
+        hendelse: SøknadInnsendtHendelse,
+    ): Behandling {
+        var fomTom = LocalDate.now()
+        var forrigeBehandling: Behandling? = null
+        for (nummer in 1..antall) {
+            val behandling =
+                Behandling(
+                    behandler = hendelse,
+                    opplysninger = listOf(Faktum(tidligereOpplysning, nummer.toDouble(), Gyldighetsperiode(fomTom, fomTom))),
+                    basertPå = forrigeBehandling?.let { listOf(it) } ?: emptyList(),
+                )
+            forrigeBehandling = behandling
+            // TODO: Det burde eksplodere uten denne
+            //  fomTom = fomTom.plusDays(1)
+        }
+        return forrigeBehandling!!
     }
 
     @Test
