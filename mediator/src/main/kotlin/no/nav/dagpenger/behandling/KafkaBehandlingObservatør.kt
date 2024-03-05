3222f70360c9e0f3ed5a5d86fffc1f623cc1f497
Geir André Lund <geir.andre.lund@nav.no>
2024-03-05 09:55:18 +0100

    Logg når vi sender behandling eventer.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/KafkaBehandlingObservatør.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/KafkaBehandlingObservatør.kt
@@ -1,34 +1,59 @@
 package no.nav.dagpenger.behandling
 
+import mu.withLoggingContext
 import no.nav.dagpenger.behandling.modell.BehandlingObservatør
 import no.nav.dagpenger.behandling.modell.PersonObservatør
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
+import no.nav.helse.rapids_rivers.withMDC
+import kotlin.math.log
 
 internal class KafkaBehandlingObservatør(private val messageContext: MessageContext) : PersonObservatør {
+
+    private companion object {
+        val logger = mu.KotlinLogging.logger {}
+    }
+
     override fun behandlingOpprettet(behandlingOpprettet: BehandlingObservatør.BehandlingOpprettet) {
-        val event =
-            JsonMessage.newMessage(
-                eventName = "behandling_opprettet",
-                mapOf(
-                    "ident" to behandlingOpprettet.ident,
-                    "behandlingId" to behandlingOpprettet.behandlingId,
-                    "søknadId" to behandlingOpprettet.søknadId,
-                ),
+
+        withLoggingContext(
+            mapOf(
+                "behandlingId" to behandlingOpprettet.behandlingId.toString(),
+                "søknadId" to behandlingOpprettet.søknadId.toString()
             )
-        messageContext.publish(behandlingOpprettet.ident, event.toJson())
+        ) {
+            val event =
+                JsonMessage.newMessage(
+                    eventName = "behandling_opprettet",
+                    mapOf(
+                        "ident" to behandlingOpprettet.ident,
+                        "behandlingId" to behandlingOpprettet.behandlingId,
+                        "søknadId" to behandlingOpprettet.søknadId,
+                    ),
+                )
+            messageContext.publish(behandlingOpprettet.ident, event.toJson())
+            logger.info { "Sendt 'behandling_opprettet'" }
+        }
     }
 
     override fun behandlingAvsluttet(behandlingAvsluttet: BehandlingObservatør.BehandlingAvsluttet) {
-        val event =
-            JsonMessage.newMessage(
-                "behandling_avsluttet",
-                mapOf(
-                    "ident" to behandlingAvsluttet.ident,
-                    "behandlingId" to behandlingAvsluttet.behandlingId,
-                    "søknadId" to behandlingAvsluttet.søknadId,
-                ),
+        withLoggingContext(
+            mapOf(
+                "behandlingId" to behandlingAvsluttet.behandlingId.toString(),
+                "søknadId" to behandlingAvsluttet.søknadId.toString()
             )
-        messageContext.publish(behandlingAvsluttet.ident, event.toJson())
+        ) {
+            val event =
+                JsonMessage.newMessage(
+                    "behandling_avsluttet",
+                    mapOf(
+                        "ident" to behandlingAvsluttet.ident,
+                        "behandlingId" to behandlingAvsluttet.behandlingId,
+                        "søknadId" to behandlingAvsluttet.søknadId,
+                    ),
+                )
+            messageContext.publish(behandlingAvsluttet.ident, event.toJson())
+            logger.info { "Sendt 'behandling_avsluttet'" }
+        }
     }
 }
