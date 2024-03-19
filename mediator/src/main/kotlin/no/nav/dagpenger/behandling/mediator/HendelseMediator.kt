cd8a216e65ee06300a935583ae6253af710548a3
André Roaldseth <andre.roaldseth@nav.no>
2024-03-19 14:57:32 +0100

    Skru av clean

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/HendelseMediator.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/HendelseMediator.kt
@@ -5,10 +5,12 @@ import io.opentelemetry.api.common.Attributes
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.instrumentation.annotations.WithSpan
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.dagpenger.aktivitetslogg.aktivitet.Hendelse
 import no.nav.dagpenger.behandling.modell.hendelser.PersonHendelse
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.RapidsConnection
+import java.util.UUID
 
 class HendelseMediator(private val rapidsConnection: RapidsConnection) {
     private companion object {
@@ -31,11 +33,15 @@ class HendelseMediator(private val rapidsConnection: RapidsConnection) {
 
         hendelsestyper.forEach { (type, hendelse) ->
             val data = hendelse.detaljer() + hendelse.kontekst()
-            val melding = JsonMessage.newMessage(type.name, data)
-            sikkerlogg.info { "sender hendelse ${type.name}:\n${melding.toJson()}}" }
-            logger.info { "sender hendelse for ${type.name}" }
-            Span.current().addEvent("Publiserer hendelse", Attributes.of(AttributeKey.stringKey("hendelse"), type.name))
-            rapidsConnection.publish(personhendelse.ident(), melding.toJson())
+            val kontekst = data.filterValues { it is String || it is UUID }.map { (key, value) -> key to value.toString() }
+
+            withLoggingContext(*kontekst.toTypedArray()) {
+                val melding = JsonMessage.newMessage(type.name, data)
+                sikkerlogg.info { "sender hendelse ${type.name}:\n${melding.toJson()}}" }
+                logger.info { "sender hendelse for ${type.name}" }
+                Span.current().addEvent("Publiserer hendelse", Attributes.of(AttributeKey.stringKey("hendelse"), type.name))
+                rapidsConnection.publish(personhendelse.ident(), melding.toJson())
+            }
         }
     }
 }
