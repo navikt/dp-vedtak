42210e2dd1caa4e4a783984113e280cb880daeac
André Roaldseth <andre.roaldseth@nav.no>
2024-03-20 13:24:49 +0100

    Clean databasen ved startup
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/ApplicationBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/ApplicationBuilder.kt
@@ -2,6 +2,7 @@ package no.nav.dagpenger.behandling.mediator
 
 import mu.KotlinLogging
 import no.nav.dagpenger.behandling.db.PostgresDataSourceBuilder
+import no.nav.dagpenger.behandling.db.PostgresDataSourceBuilder.clean
 import no.nav.dagpenger.behandling.db.PostgresDataSourceBuilder.runMigration
 import no.nav.dagpenger.behandling.mediator.api.behandlingApi
 import no.nav.dagpenger.behandling.mediator.melding.PostgresHendelseRepository
@@ -32,13 +33,13 @@ internal class ApplicationBuilder(config: Map<String, String>) : RapidsConnectio
         MessageMediator(
             rapidsConnection = rapidsConnection,
             personMediator =
-                PersonMediator(
-                    personRepository = personRepository,
-                    aktivitetsloggMediator = AktivitetsloggMediator(rapidsConnection),
-                    behovMediator = BehovMediator(rapidsConnection),
-                    hendelseMediator = HendelseMediator(rapidsConnection),
-                    observatører = emptySet(),
-                ),
+            PersonMediator(
+                personRepository = personRepository,
+                aktivitetsloggMediator = AktivitetsloggMediator(rapidsConnection),
+                behovMediator = BehovMediator(rapidsConnection),
+                hendelseMediator = HendelseMediator(rapidsConnection),
+                observatører = emptySet(),
+            ),
             hendelseRepository = PostgresHendelseRepository(PostgresDataSourceBuilder.dataSource),
         )
 
@@ -60,7 +61,7 @@ internal class ApplicationBuilder(config: Map<String, String>) : RapidsConnectio
     fun stop() = rapidsConnection.stop()
 
     override fun onStartup(rapidsConnection: RapidsConnection) {
-        // clean()
+        clean()
         runMigration()
         logger.info { "Starter opp dp-behandling" }
     }
