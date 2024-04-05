7bdeda466eed8338e0a753ff4241a9408faa1a7c
André Roaldseth <andre.roaldseth@nav.no>
2024-04-05 19:36:03 +0200

    Lag en test som printer ut hele regeltreet

---
+++ dagpenger/src/test/kotlin/no/nav/dagpenger/regel/DiagramTest.kt
@@ -0,0 +1,29 @@
+package no.nav.dagpenger.regel
+
+import no.nav.dagpenger.opplysning.dag.RegeltreBygger
+import no.nav.dagpenger.opplysning.dag.printer.MermaidPrinter
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DiagramTest {
+    @Test
+    fun `printer hele dagpengeregeltreet`() {
+        val bygger =
+            RegeltreBygger(
+                Alderskrav.regelsett,
+                Meldeplikt.regelsett,
+                Minsteinntekt.regelsett,
+                Opptjeningstid.regelsett,
+                ReellArbeidssøker.regelsett,
+                RettTilDagpenger.regelsett,
+                Søknadstidspunkt.regelsett,
+            )
+
+        val regeltre = bygger.dag()
+        val mermaidPrinter = MermaidPrinter(regeltre)
+        val output = mermaidPrinter.toPrint()
+        assertTrue(output.contains("graph RL"))
+
+        println(output)
+    }
+}
