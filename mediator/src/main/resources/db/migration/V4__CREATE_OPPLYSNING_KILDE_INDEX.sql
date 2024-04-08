04ee874f353dfd5f3cca8a2e6c81280a082e2d33
Geir André Lund <geir.andre.lund@nav.no>
2024-04-08 18:29:13 +0200

    Lag database index for kilde på opplysningens ID.

---
+++ mediator/src/main/resources/db/migration/V4__CREATE_OPPLYSNING_KILDE_INDEX.sql
@@ -0,0 +1,2 @@
+CREATE INDEX IF NOT EXISTS idx_opplysning_kilde ON opplysning_kilde(opplysning_id);
+CREATE INDEX IF NOT EXISTS idx_behandling_opplysninger ON behandling_opplysninger(behandling_id);
