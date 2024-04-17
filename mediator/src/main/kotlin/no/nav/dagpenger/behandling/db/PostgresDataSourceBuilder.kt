ac75769f371e09ab32a2400636796587f35e20ba
André Roaldseth <andre.roaldseth@nav.no>
2024-04-17 15:18:40 +0200

    Bruk JDBC URL fra NAIS
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/db/PostgresDataSourceBuilder.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/db/PostgresDataSourceBuilder.kt
@@ -11,20 +11,15 @@ import org.flywaydb.core.internal.configuration.ConfigUtils
 internal object PostgresDataSourceBuilder {
     const val DB_USERNAME_KEY = "DB_USERNAME"
     const val DB_PASSWORD_KEY = "DB_PASSWORD"
-    const val DB_DATABASE_KEY = "DB_DATABASE"
-    const val DB_HOST_KEY = "DB_HOST"
-    const val DB_PORT_KEY = "DB_PORT"
+    const val DB_URL_KEY = "DB_URL"
 
     private fun getOrThrow(key: String): String = getEnv(key) ?: getSystemProperty(key)
 
     val dataSource by lazy {
         HikariDataSource().apply {
-            dataSourceClassName = "org.postgresql.ds.PGSimpleDataSource"
-            addDataSourceProperty("serverName", getOrThrow(DB_HOST_KEY))
-            addDataSourceProperty("portNumber", getOrThrow(DB_PORT_KEY))
-            addDataSourceProperty("databaseName", getOrThrow(DB_DATABASE_KEY))
-            addDataSourceProperty("user", getOrThrow(DB_USERNAME_KEY))
-            addDataSourceProperty("password", getOrThrow(DB_PASSWORD_KEY))
+            jdbcUrl = getOrThrow(DB_URL_KEY)
+            username = getOrThrow(DB_USERNAME_KEY)
+            password = getOrThrow(DB_PASSWORD_KEY)
             maximumPoolSize = 10
             minimumIdle = 1
             idleTimeout = 10001
--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/Postgres.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/Postgres.kt
@@ -27,22 +27,15 @@ internal object Postgres {
 
     fun setup() {
         System.setProperty(ConfigUtils.CLEAN_DISABLED, "false")
-        System.setProperty(PostgresDataSourceBuilder.DB_HOST_KEY, instance.host)
-        System.setProperty(
-            PostgresDataSourceBuilder.DB_PORT_KEY,
-            instance.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT).toString(),
-        )
-        System.setProperty(PostgresDataSourceBuilder.DB_DATABASE_KEY, instance.databaseName)
-        System.setProperty(PostgresDataSourceBuilder.DB_PASSWORD_KEY, instance.password)
+        System.setProperty(PostgresDataSourceBuilder.DB_URL_KEY, instance.jdbcUrl)
         System.setProperty(PostgresDataSourceBuilder.DB_USERNAME_KEY, instance.username)
+        System.setProperty(PostgresDataSourceBuilder.DB_PASSWORD_KEY, instance.password)
     }
 
     fun tearDown() {
-        System.clearProperty(PostgresDataSourceBuilder.DB_PASSWORD_KEY)
+        System.clearProperty(PostgresDataSourceBuilder.DB_URL_KEY)
         System.clearProperty(PostgresDataSourceBuilder.DB_USERNAME_KEY)
-        System.clearProperty(PostgresDataSourceBuilder.DB_HOST_KEY)
-        System.clearProperty(PostgresDataSourceBuilder.DB_PORT_KEY)
-        System.clearProperty(PostgresDataSourceBuilder.DB_DATABASE_KEY)
+        System.clearProperty(PostgresDataSourceBuilder.DB_PASSWORD_KEY)
         System.clearProperty(ConfigUtils.CLEAN_DISABLED)
     }
 
