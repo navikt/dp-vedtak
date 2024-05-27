d46f1f0a1b0455e8effe390d713ac79ea59c5097
André Roaldseth <andre.roaldseth@nav.no>
2024-05-27 12:51:35 +0200

    Lag rekursive regelsett med kriterier for om de skal med
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Elin Vethe<elin.vethe@nav.no>
    Co-authored-by: Rudi Wei Yu <rudi.wei.yu@nav.no>

--- opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelsett.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelsett.kt
@@ -5,12 +5,23 @@ import java.time.LocalDate
 
 class Regelsett(val navn: String, block: Regelsett.() -> Unit = {}) {
     private val regler: MutableMap<Opplysningstype<*>, TemporalCollection<Regel<*>>> = mutableMapOf()
+    private var kriterie: Kriterie = { true }
+    private val regelsett: MutableList<Regelsett> = mutableListOf()
 
     init {
         block()
     }
 
-    fun regler(forDato: LocalDate = LocalDate.MIN) = regler.map { it.value.get(forDato) }.toList()
+    fun regler(
+        forDato: LocalDate = LocalDate.MIN,
+        opplysninger: Opplysninger? = null,
+    ): List<Regel<*>> {
+        // TODO: Rydd opp i rekursiviteten her
+        if (opplysninger != null && !skalVurderes(opplysninger)) return emptyList()
+        return regler.map { it.value.get(forDato) }.toList() +
+            regelsett.filter { opplysninger == null || it.skalVurderes(opplysninger) }
+                .flatMap { it.regler(forDato) }
+    }
 
     private fun leggTil(
         gjelderFra: LocalDate,
@@ -22,4 +33,19 @@ class Regelsett(val navn: String, block: Regelsett.() -> Unit = {}) {
         gjelderFraOgMed: LocalDate = LocalDate.MIN,
         block: Opplysningstype<T>.() -> Regel<*>,
     ) = leggTil(gjelderFraOgMed, produserer.block())
+
+    fun skalVurderes(block: Kriterie) {
+        kriterie = block
+    }
+
+    fun skalVurderes(opplysninger: Opplysninger): Boolean = kriterie(opplysninger)
+
+    fun regelsett(
+        navn: String,
+        block: Regelsett.() -> Unit,
+    ) {
+        regelsett.add(Regelsett(navn, block))
+    }
 }
+
+typealias Kriterie = Opplysninger.() -> Boolean
