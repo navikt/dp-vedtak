d9a61248036e011e5409bf197067e7c49bb73517
Geir André Lund <geir.andre.lund@nav.no>
2024-05-30 12:35:01 +0200

    Definerer regelsett for hendelsen SøknadInnsendtHendelse ett sted.

---
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/regel/SøknadinnsendtRegelsett.kt
@@ -0,0 +1,16 @@
+package no.nav.dagpenger.regel
+
+object SøknadinnsendtRegelsett {
+    val regelsett =
+        listOf(
+            Alderskrav.regelsett,
+            Meldeplikt.regelsett,
+            Minsteinntekt.regelsett,
+            Opptjeningstid.regelsett,
+            ReellArbeidssøker.regelsett,
+            RettTilDagpenger.regelsett,
+            Rettighetstype.regelsett,
+            Søknadstidspunkt.regelsett,
+            Virkningstidspunkt.regelsett,
+        )
+}
--- modell/src/main/kotlin/no/nav/dagpenger/behandling/modell/hendelser/SøknadInnsendtHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/behandling/modell/hendelser/SøknadInnsendtHendelse.kt
@@ -3,15 +3,8 @@ package no.nav.dagpenger.behandling.modell.hendelser
 import no.nav.dagpenger.behandling.modell.Behandling
 import no.nav.dagpenger.opplysning.Faktum
 import no.nav.dagpenger.opplysning.Opplysningstype
-import no.nav.dagpenger.regel.Alderskrav
-import no.nav.dagpenger.regel.Meldeplikt
-import no.nav.dagpenger.regel.Minsteinntekt
-import no.nav.dagpenger.regel.Opptjeningstid
-import no.nav.dagpenger.regel.ReellArbeidssøker
 import no.nav.dagpenger.regel.RettTilDagpenger
-import no.nav.dagpenger.regel.Rettighetstype
-import no.nav.dagpenger.regel.Søknadstidspunkt
-import no.nav.dagpenger.regel.Virkningstidspunkt
+import no.nav.dagpenger.regel.SøknadinnsendtRegelsett
 import java.time.LocalDate
 import java.util.UUID
 
@@ -22,18 +15,7 @@ class SøknadInnsendtHendelse(
     gjelderDato: LocalDate,
     fagsakId: Int,
 ) : StartHendelse(meldingsreferanseId, ident, SøknadId(søknadId), gjelderDato, fagsakId) {
-    override fun regelsett() =
-        listOf(
-            Alderskrav.regelsett,
-            Meldeplikt.regelsett,
-            Minsteinntekt.regelsett,
-            Opptjeningstid.regelsett,
-            ReellArbeidssøker.regelsett,
-            RettTilDagpenger.regelsett,
-            Rettighetstype.regelsett,
-            Søknadstidspunkt.regelsett,
-            Virkningstidspunkt.regelsett,
-        )
+    override fun regelsett() = SøknadinnsendtRegelsett.regelsett
 
     override fun avklarer(): Opplysningstype<Boolean> = RettTilDagpenger.kravPåDagpenger
 
