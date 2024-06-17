9b0a4599ba6c0af22361e58ac393e4e27ae0e48a
André Roaldseth <andre.roaldseth@nav.no>
2024-06-17 12:16:31 +0200

    Revert "Forenkle måten vi finner regler som skal med"
    This reverts commit e72e157c561d46a562be4c72ff217f5aa92fa2fe.

--- opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelsett.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelsett.kt
@@ -3,7 +3,10 @@ package no.nav.dagpenger.opplysning
 import no.nav.dagpenger.opplysning.regel.Regel
 import java.time.LocalDate
 
-class Regelsett(val navn: String, block: Regelsett.() -> Unit = {}) {
+class Regelsett(
+    val navn: String,
+    block: Regelsett.() -> Unit = {},
+) {
     private val regler: MutableMap<Opplysningstype<*>, TemporalCollection<Regel<*>>> = mutableMapOf()
 
     init {
