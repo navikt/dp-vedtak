4a1c9bbe2ead421c7e08166968dd076784f05aaf
Geir André Lund <geir.andre.lund@nav.no>
2024-06-27 12:28:48 +0200

    Trenger ikke å logge ider i meldinger, legger de i loggekontekst i stedet.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/AvklaringKafkaObservatør.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/AvklaringKafkaObservatør.kt
@@ -1,6 +1,7 @@
 package no.nav.dagpenger.behandling.mediator.repository
 
 import mu.KotlinLogging
+import mu.withLoggingContext
 import no.nav.helse.rapids_rivers.JsonMessage
 import no.nav.helse.rapids_rivers.MessageContext
 
@@ -8,21 +9,26 @@ class AvklaringKafkaObservatør(
     private val rapid: MessageContext,
 ) : AvklaringRepositoryObserver {
     override fun nyAvklaring(nyAvklaringHendelse: AvklaringRepositoryObserver.NyAvklaringHendelse) {
-        rapid.publish(
-            nyAvklaringHendelse.ident,
-            JsonMessage
-                .newMessage(
-                    "NyAvklaring",
-                    mapOf<String, Any>(
-                        "ident" to nyAvklaringHendelse.ident,
-                        "avklaringId" to nyAvklaringHendelse.avklaring.id,
-                        "kode" to nyAvklaringHendelse.avklaring.kode.kode,
-                    ) + nyAvklaringHendelse.kontekst.kontekstMap,
-                ).toJson(),
-        )
+        withLoggingContext(
+            "avklaringId" to nyAvklaringHendelse.avklaring.id.toString(),
+            "kode" to nyAvklaringHendelse.avklaring.kode.kode,
+        ) {
+            rapid.publish(
+                nyAvklaringHendelse.ident,
+                JsonMessage
+                    .newMessage(
+                        "NyAvklaring",
+                        mapOf<String, Any>(
+                            "ident" to nyAvklaringHendelse.ident,
+                            "avklaringId" to nyAvklaringHendelse.avklaring.id,
+                            "kode" to nyAvklaringHendelse.avklaring.kode.kode,
+                        ) + nyAvklaringHendelse.kontekst.kontekstMap,
+                    ).toJson(),
+            )
 
-        logger.info {
-            "Publisert NyAvklaring med kode ${nyAvklaringHendelse.avklaring.kode.kode} for avklaringId=${nyAvklaringHendelse.avklaring.id}"
+            logger.info {
+                "Publisert NyAvklaring med kode ${nyAvklaringHendelse.avklaring.kode.kode}"
+            }
         }
     }
 
