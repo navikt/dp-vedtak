7fc7a6bba0146514d01c439b8d637f5e572fbc21
Geir André Lund <geir.andre.lund@nav.no>
2024-08-01 12:22:48 +0200

    Skriv regeltre for alle regelsettene til Dagpenger inngangsvilkår.

--- dagpenger/src/test/kotlin/no/nav/dagpenger/regel/DiagramTest.kt
+++ dagpenger/src/test/kotlin/no/nav/dagpenger/regel/DiagramTest.kt
@@ -1,26 +1,30 @@
 package no.nav.dagpenger.regel
 
+import com.spun.util.persistence.Loader
 import no.nav.dagpenger.dag.printer.MermaidPrinter
 import no.nav.dagpenger.opplysning.Regelverk
 import no.nav.dagpenger.opplysning.dag.RegeltreBygger
+import org.approvaltests.Approvals
+import org.approvaltests.core.Options
+import org.approvaltests.namer.NamerWrapper
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import java.nio.file.Paths
 
 class DiagramTest {
+    private companion object {
+        val path = "${
+            Paths.get("").toAbsolutePath().toString().substringBeforeLast("/")
+        }/docs/"
+        val options = Options().forFile().withExtension(".md")
+    }
+
     @Test
     fun `printer hele dagpengeregeltreet`() {
         val bygger =
             RegeltreBygger(
-                Alderskrav.regelsett,
-                Meldeplikt.regelsett,
-                Minsteinntekt.regelsett,
-                Opptjeningstid.regelsett,
-                ReellArbeidssøker.regelsett,
-                KravPåDagpenger.regelsett,
-                Rettighetstype.regelsett,
-                Søknadstidspunkt.regelsett,
-                TapAvArbeidsinntektOgArbeidstid.regelsett,
-                Verneplikt.regelsett,
+                *RegelverkDagpenger.regelsett.toTypedArray(),
             )
 
         val regeltre = bygger.dag()
@@ -29,27 +33,42 @@ class DiagramTest {
         assertTrue(output.contains("graph RL"))
 
         println(output)
+
+        @Language("Markdown")
+        val markdown =
+            """
+                    ># Regeltre - Dagpenger 
+                    >
+                    >## Regeltre
+                    >
+                    >```mermaid
+                    >${output.trim()}
+                    >```
+                    """.trimMargin(">")
+        skriv(
+            markdown,
+        )
     }
 
     @Test
     fun `lager tre av regelsettene`() {
         val regelverk =
             Regelverk(
-                Alderskrav.regelsett,
-                Meldeplikt.regelsett,
-                Minsteinntekt.regelsett,
-                Opptjeningstid.regelsett,
-                ReellArbeidssøker.regelsett,
-                KravPåDagpenger.regelsett,
-                Rettighetstype.regelsett,
-                Søknadstidspunkt.regelsett,
-                TapAvArbeidsinntektOgArbeidstid.regelsett,
-                Verneplikt.regelsett,
+                *RegelverkDagpenger.regelsett.toTypedArray(),
             )
 
         regelverk.regeltreFor(KravPåDagpenger.kravPåDagpenger).also {
             val b = MermaidPrinter(it)
             println(b.toPrint())
         }
+    }
+
+    fun skriv(dokumentasjon: String) {
+        Approvals.namerCreater = Loader { NamerWrapper({ "regeltre-dagpenger" }, { path }) }
+        Approvals
+            .verify(
+                dokumentasjon,
+                options,
+            )
     }
 }
