7b6ecdfb6049c917e40cdb410386a4aefda4a3d2
André Roaldseth <andre.roaldseth@nav.no>
2024-08-05 14:42:47 +0200

    Fjern ubrukte entitetsklasser
    Vi lager de nok på nytt ganske snart
    
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>

--- mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/PostgresMigrationTest.kt
+++ mediator/src/test/kotlin/no/nav/dagpenger/behandling/db/PostgresMigrationTest.kt
@@ -10,7 +10,7 @@ class PostgresMigrationTest {
     fun `Migration scripts are applied successfully`() {
         withCleanDb {
             val migrations = runMigration()
-            migrations shouldBeExactly 11
+            migrations shouldBeExactly 12
         }
     }
 }
