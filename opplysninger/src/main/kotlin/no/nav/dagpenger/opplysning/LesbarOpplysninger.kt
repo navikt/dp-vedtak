8c5fe92206be461888ab0a29197d59cd81d647a7
André Roaldseth <andre.roaldseth@nav.no>
2024-08-09 13:01:01 +0200

    Fjern ubrukt metode for mangler()

--- opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/LesbarOpplysninger.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/LesbarOpplysninger.kt
@@ -9,8 +9,6 @@ interface LesbarOpplysninger {
 
     fun har(opplysningstype: Opplysningstype<*>): Boolean
 
-    fun mangler(opplysningstype: Opplysningstype<*>): Boolean = !har(opplysningstype)
-
     fun finnAlle(opplysningstyper: List<Opplysningstype<*>>): List<Opplysning<*>>
 
     fun finnAlle(): List<Opplysning<*>>
