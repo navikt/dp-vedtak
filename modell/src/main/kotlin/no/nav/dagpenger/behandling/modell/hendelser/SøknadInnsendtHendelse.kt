f6b5ef151ca73545c77c55feb7aa26177decd156
André Roaldseth <andre.roaldseth@nav.no>
2024-08-27 14:37:11 +0200

    Legg til sjekk av feature flag på fastsettelser i regelkjøringen
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: Mette Wendy Lindberg Gulbrandsen <mette.wendy.lindberg.gulbrandsen@nav.no>

--- modell/src/main/kotlin/no/nav/dagpenger/behandling/modell/hendelser/SøknadInnsendtHendelse.kt
+++ modell/src/main/kotlin/no/nav/dagpenger/behandling/modell/hendelser/SøknadInnsendtHendelse.kt
@@ -1,5 +1,6 @@
 package no.nav.dagpenger.behandling.modell.hendelser
 
+import no.nav.dagpenger.behandling.konfigurasjon.støtterInnvilgelse
 import no.nav.dagpenger.behandling.modell.Behandling
 import no.nav.dagpenger.opplysning.Faktum
 import no.nav.dagpenger.opplysning.LesbarOpplysninger
@@ -48,7 +49,7 @@ class SøknadInnsendtHendelse(
             opplysninger.har(KravPåDagpenger.kravPåDagpenger) && opplysninger.finnOpplysning(KravPåDagpenger.kravPåDagpenger).verdi
 
         val regelsettFor = regelsettFor(opplysningstype).toMutableSet()
-        if (harKravPåDagpenger) {
+        if (harKravPåDagpenger && støtterInnvilgelse) {
             val fastsetting =
                 RegelverkDagpenger.regelsettFor(Dagpengeperiode.antallStønadsuker) +
                     RegelverkDagpenger.regelsettFor(Dagpengegrunnlag.grunnlag) +
