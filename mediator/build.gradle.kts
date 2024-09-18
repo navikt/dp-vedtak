8aada236d2bd1e42d2872778a6dc3596a8badee2
Geir André Lund <geir.andre.lund@nav.no>
2024-09-18 09:45:53 +0200

    Endret eiersted til shadow-jar pluginen.

--- mediator/build.gradle.kts
+++ mediator/build.gradle.kts
@@ -2,7 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     id("common")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "8.3.1"
     application
     kotlin("plugin.serialization") version "2.0.20"
 }
