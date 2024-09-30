652db49ae311b49a43a93bffece68b4d1bb01420
Geir André Lund <geir.andre.lund@nav.no>
2024-09-30 08:17:30 +0200

    Bruk cucumber junit engine

--- dagpenger/src/test/kotlin/no/nav/dagpenger/features/utils/RunCucumberTest.kt
+++ dagpenger/src/test/kotlin/no/nav/dagpenger/features/utils/RunCucumberTest.kt
@@ -4,11 +4,13 @@ import io.cucumber.core.options.Constants.FILTER_TAGS_PROPERTY_NAME
 import io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME
 import io.cucumber.core.options.Constants.PLUGIN_PUBLISH_ENABLED_PROPERTY_NAME
 import org.junit.platform.suite.api.ConfigurationParameter
+import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
 
 @Suite
 @SelectClasspathResource("features")
+@IncludeEngines("cucumber") //
 @ConfigurationParameter(
     key = PLUGIN_PROPERTY_NAME,
     value = "pretty, html:build/reports/cucumber.html, no.nav.dagpenger.features.utils.RegeltreDokumentasjonPlugin",
