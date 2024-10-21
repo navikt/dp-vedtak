798b4c618e1a3b10661d039bd5805afc3e50fcc7
André Roaldseth <andre.roaldseth@nav.no>
2024-10-21 16:55:04 +0200

    Fjern logging av opplysninger

--- opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Opplysninger.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Opplysninger.kt
@@ -100,8 +100,6 @@ class Opplysninger private constructor(
             throw IllegalStateException(
                 """Har mer enn 1 opplysning av type $opplysningstype i opplysningerId=$id.
                 |Fant ${alleOpplysninger.count { it.er(opplysningstype) }} duplikater blant ${alleOpplysninger.size} opplysninger.
-                |Opplysningene er:
-                |${alleOpplysninger.joinToString("\n") { "id: ${it.id} ," + it.toString() }}
                 |Basert på (${basertPåOpplysninger.size} sett) ${basertPåOpplysninger.joinToString { it.id.toString() }}
                 """.trimMargin(),
             )
