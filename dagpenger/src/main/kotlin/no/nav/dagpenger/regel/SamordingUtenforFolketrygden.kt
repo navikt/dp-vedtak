5693b9c3d86de604ab37c3c4790cc00d2fd0a10d
André Roaldseth <andre.roaldseth@nav.no>
2024-11-21 11:10:14 +0100

    Regn ut nedre terskel for samordning
    Co-authored-by: Geir André Lund <geir.andre.lund@nav.no>
    Co-authored-by: Richard Andre Martinsen <richard.martinsen@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>

--- dagpenger/src/main/kotlin/no/nav/dagpenger/regel/SamordingUtenforFolketrygden.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/regel/SamordingUtenforFolketrygden.kt
@@ -6,9 +6,11 @@ import no.nav.dagpenger.opplysning.Regelsett
 import no.nav.dagpenger.opplysning.id
 import no.nav.dagpenger.opplysning.regel.enAv
 import no.nav.dagpenger.opplysning.regel.innhentes
+import no.nav.dagpenger.opplysning.regel.multiplikasjon
 import no.nav.dagpenger.opplysning.regel.oppslag
 import no.nav.dagpenger.regel.Behov.AndreØkonomiskeYtelser
 import no.nav.dagpenger.regel.Behov.OppgittAndreYtelserUtenforNav
+import no.nav.dagpenger.regel.Minsteinntekt.grunnbeløp
 import no.nav.dagpenger.regel.Søknadstidspunkt.prøvingsdato
 
 object SamordingUtenforFolketrygden {
@@ -25,11 +27,16 @@ object SamordingUtenforFolketrygden {
             "Mottar andre økonomiske ytelser fra arbeidsgiver eller tidligere arbeidsgiver enn lønn".id(AndreØkonomiskeYtelser),
         )
 
+    val terskelVedSamordning = Opplysningstype.somDesimaltall("Hvor mange prosent av G skal brukes som terskel ved samordning")
+    val treProsentG = Opplysningstype.somBeløp("Beløp tilsvarende nedre terskel av G")
     val skalSamordnesUtenforFolketrygden = Opplysningstype.somBoolsk("Skal samordnes med ytelser utenfor folketrygden")
 
     val regelsett =
         Regelsett("§ 4-26.Samordning med ytelser utenfor folketrygden") {
             regel(andreYtelser) { innhentes }
+
+            regel(terskelVedSamordning) { oppslag(prøvingsdato) { 0.03 } }
+            regel(treProsentG) { multiplikasjon(grunnbeløp, terskelVedSamordning) }
 
             regel(pensjonFraOffentligTjenestepensjonsordning) { oppslag(prøvingsdato) { false } }
             regel(redusertUførepensjon) { oppslag(prøvingsdato) { false } }
