fa08645aae7009521d4ba54e6f40a1f3361d72f4
André Roaldseth <andre.roaldseth@nav.no>
2024-12-17 11:06:32 +0100

    Legg til feilssøking på når regelkjøring kjører regler men mangler avhengigheter
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>

--- opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelkjøring.kt
+++ opplysninger/src/main/kotlin/no/nav/dagpenger/opplysning/Regelkjøring.kt
@@ -163,10 +163,25 @@ class Regelkjøring(
     }
 
     private fun kjør(regel: Regel<*>) {
-        val opplysning = regel.lagProdukt(opplysningerPåPrøvingsdato)
-        kjørteRegler.add(regel)
-        plan.remove(regel)
-        opplysninger.leggTilUtledet(opplysning)
+        try {
+            val opplysning = regel.lagProdukt(opplysningerPåPrøvingsdato)
+            kjørteRegler.add(regel)
+            plan.remove(regel)
+            opplysninger.leggTilUtledet(opplysning)
+        } catch (e: IllegalArgumentException) {
+            if (e.message?.startsWith("Prøver å kjøre") == true) {
+                throw IllegalArgumentException(
+                    """Lager produkt, men mangler avhengigheter. 
+                    |KjørteRegler:
+                    |${kjørteRegler.joinToString("\n")}
+                    |Plan:
+                    |${plan.joinToString("\n")}
+                    """.trimMargin(),
+                    e,
+                )
+            }
+            throw e
+        }
     }
 
     private fun trenger(): Set<Opplysningstype<*>> {
--- opplysninger/src/test/kotlin/no/nav/dagpenger/opplysning/RegelkjøringTest.kt
+++ opplysninger/src/test/kotlin/no/nav/dagpenger/opplysning/RegelkjøringTest.kt
@@ -1,11 +1,17 @@
 package no.nav.dagpenger.opplysning
 
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldNotBeEmpty
+import io.kotest.matchers.shouldBe
 import no.nav.dagpenger.opplysning.TestOpplysningstyper.a
 import no.nav.dagpenger.opplysning.TestOpplysningstyper.b
 import no.nav.dagpenger.opplysning.TestOpplysningstyper.c
 import no.nav.dagpenger.opplysning.regel.enAv
+import no.nav.dagpenger.opplysning.regel.innhentes
+import no.nav.dagpenger.opplysning.regel.minstAv
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
 
 class RegelkjøringTest {
     @Test
@@ -22,5 +28,66 @@ class RegelkjøringTest {
         assertThrows<IllegalArgumentException> {
             Regelkjøring(1.mai, Opplysninger(), regelsett1, regelsett2)
         }
+    }
+
+    @Test
+    fun `regelkjøring håndterer nye regler og opplysninger i påbegynte behandlinger`() {
+        val opplysningA = Opplysningstype.somDesimaltall("opplysningA")
+        val opplysningB = Opplysningstype.somDesimaltall("opplysningB")
+        val opplysningC = Opplysningstype.somDesimaltall("opplysningC")
+        val opplysningD = Opplysningstype.somDesimaltall("opplysningD")
+
+        val opplysninger = Opplysninger()
+        val regelsett1 =
+            Regelsett("regelsett") {
+                regel(opplysningB) { innhentes }
+                regel(opplysningC) { innhentes }
+                regel(opplysningA) { minstAv(opplysningB) }
+            }
+        opplysninger.leggTil(Faktum(opplysningB, 1.5))
+        opplysninger.leggTil(Faktum(opplysningC, 0.5))
+
+        // Kjør originale regler
+        Regelkjøring(LocalDate.now(), opplysninger, regelsett1).apply {
+            val rapport = evaluer()
+
+            rapport.mangler.shouldBeEmpty()
+            rapport.kjørteRegler.shouldNotBeEmpty()
+
+            opplysninger.finnOpplysning(opplysningB).verdi shouldBe 1.5
+            opplysninger.finnOpplysning(opplysningA).verdi shouldBe 1.5
+        }
+
+        // Endring i reglene
+        val regelsett2 =
+            Regelsett("regelsett") {
+                regel(opplysningB) { innhentes }
+                regel(opplysningC) { innhentes }
+                regel(opplysningD) { minstAv(opplysningC) }
+                regel(opplysningA) { minstAv(opplysningB, opplysningD) }
+            }
+
+        // Kjør nye regler med nye opplysninger
+        Regelkjøring(LocalDate.now(), opplysninger, regelsett2).apply {
+            val rapport = evaluer()
+
+            // rapport.mangler.shouldContain(opplysningD)
+
+            opplysninger.finnOpplysning(opplysningB).verdi shouldBe 1.5
+            opplysninger.finnOpplysning(opplysningA).verdi shouldBe 1.5
+            opplysninger.finnOpplysning(opplysningD).verdi shouldBe 0.5
+        }
+
+        /*
+        // Ikke det vi vil teste nå, men burde også fungere
+        opplysninger.leggTil(Faktum(opplysningD, 0.5))
+        Regelkjøring(LocalDate.now(), opplysninger, regelsett2).apply {
+            val rapport = evaluer()
+
+            rapport.mangler.shouldBeEmpty()
+            rapport.kjørteRegler.shouldContain(listOf(regelC, regelA))
+
+            opplysninger.finnOpplysning(opplysningC).verdi shouldBe 0.5
+        }*/
     }
 }
