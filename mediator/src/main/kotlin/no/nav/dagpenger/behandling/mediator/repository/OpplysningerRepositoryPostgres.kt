6f0d838aaf8149223e7fef422a73748ac9fb95a4
Geir André Lund <geir.andre.lund@nav.no>
2025-01-06 11:10:42 +0100

    Antall stønadsuker har byttet navn til 'Antall stønadsuker som gis ved ordinære dagpenger'
    Co-authored-by: Mette Wendy Lindberg Gulbrandsen <mette.wendy.lindberg.gulbrandsen@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>
    Co-authored-by: André Roaldseth <andre.roaldseth@nav.no>
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/OpplysningerRepositoryPostgres.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/repository/OpplysningerRepositoryPostgres.kt
@@ -35,6 +35,7 @@ import no.nav.dagpenger.opplysning.verdier.Ulid
 import no.nav.dagpenger.regel.StreikOgLockout.ikkeStreikEllerLockout
 import no.nav.dagpenger.regel.Søknadstidspunkt.søknadsdato
 import no.nav.dagpenger.regel.Søknadstidspunkt.søknadstidspunkt
+import no.nav.dagpenger.regel.fastsetting.Dagpengeperiode.ordinærPeriode
 import org.postgresql.util.PGobject
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -63,6 +64,10 @@ class OpplysningerRepositoryPostgres : OpplysningerRepository {
                 Navnebytte(
                     fra = Opplysningstype.somBoolsk("Er medlemmet påvirket av streik eller lock-out?"),
                     til = ikkeStreikEllerLockout,
+                ),
+                Navnebytte(
+                    fra = Opplysningstype.somHeltall("Antall stønadsuker"),
+                    til = ordinærPeriode,
                 ),
             )
 
