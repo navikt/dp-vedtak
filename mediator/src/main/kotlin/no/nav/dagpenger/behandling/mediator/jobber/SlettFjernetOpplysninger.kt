17af7b189217dfce226047ce422c2715f573492e
Geir André Lund <geir.andre.lund@nav.no>
2025-01-14 14:43:21 +0100

    Skru av slettejobber.
    Co-authored-by: Richard André Martinsen <richard.martinsen@nav.no>

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/jobber/SlettFjernetOpplysninger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/jobber/SlettFjernetOpplysninger.kt
@@ -18,7 +18,7 @@ internal object SlettFjernetOpplysninger {
             period = 15.minutes.inWholeMilliseconds,
             action = {
                 try {
-                    if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
+                    if (System.getenv("NAIS_CLUSTER_NAME") == "skrudd-av") {
                         vaktmesterRepository.slettOpplysninger(antall = 1)
                     } else {
                         logger.info { "Sletting av fjernet opplysninger er skrudd av" }
