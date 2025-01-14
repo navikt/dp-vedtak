7d1b607a994665422652aafe1840461c23900975
Geir André Lund <geir.andre.lund@nav.no>
2025-01-14 16:36:10 +0100

    Øke antall.

--- mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/jobber/SlettFjernetOpplysninger.kt
+++ mediator/src/main/kotlin/no/nav/dagpenger/behandling/mediator/jobber/SlettFjernetOpplysninger.kt
@@ -19,7 +19,7 @@ internal object SlettFjernetOpplysninger {
             action = {
                 try {
                     if (System.getenv("NAIS_CLUSTER_NAME") == "dev-gcp") {
-                        vaktmesterRepository.slettOpplysninger(antall = 1)
+                        vaktmesterRepository.slettOpplysninger(antall = 2)
                     } else {
                         logger.info { "Sletting av fjernet opplysninger er skrudd av" }
                     }
