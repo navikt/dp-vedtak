5a5ee4e2af61b72796fb29873101f9dd76f77c88
André Roaldseth <andre.roaldseth@nav.no>
2025-01-20 12:22:31 +0100

    Skjul mellomsteg i §4-15 periode
    Co-authored-by: Tonje Mjøvik Myran <tonje.mjovik.myran@nav.no>
    Co-authored-by: Eivind Havnelid Røyrvik <eivind.havnelid.royrvik@nav.no>

--- dagpenger/src/main/kotlin/no/nav/dagpenger/regel/fastsetting/Dagpengeperiode.kt
+++ dagpenger/src/main/kotlin/no/nav/dagpenger/regel/fastsetting/Dagpengeperiode.kt
@@ -1,6 +1,8 @@
 package no.nav.dagpenger.regel.fastsetting
 
+import no.nav.dagpenger.opplysning.Opplysningsformål.Mellomsteg
 import no.nav.dagpenger.opplysning.Opplysningstype
+import no.nav.dagpenger.opplysning.Opplysningstype.Companion.aldriSynlig
 import no.nav.dagpenger.opplysning.Regelsett
 import no.nav.dagpenger.opplysning.RegelsettType
 import no.nav.dagpenger.opplysning.regel.divisjon
@@ -15,29 +17,35 @@ import no.nav.dagpenger.regel.fastsetting.Dagpengeperiode.ordinærPeriode
 import no.nav.dagpenger.regel.folketrygden
 
 object Dagpengeperiode {
-    private val antallStønadsuker = Opplysningstype.somHeltall("Antall stønadsuker")
-    private val gjenståendeStønadsdager = Opplysningstype.somHeltall("Antall gjenstående stønadsdager")
-    val ordinærPeriode = Opplysningstype.somHeltall("Antall stønadsuker som gis ved ordinære dagpenger")
-    private val dagerIUka = Opplysningstype.somHeltall("Antall dager som skal regnes med i hver uke")
-    private val terskelFaktor12 = Opplysningstype.somDesimaltall("Terskelfaktor for 12 måneder")
-    private val terskelFaktor36 = Opplysningstype.somDesimaltall("Terskelfaktor for 36 måneder")
+    private val dagerIUka = Opplysningstype.somHeltall("Antall dager som skal regnes med i hver uke", Mellomsteg, aldriSynlig)
+
+    private val kortPeriode = Opplysningstype.somHeltall("Kort dagpengeperiode", Mellomsteg, aldriSynlig)
+    private val langPeriode = Opplysningstype.somHeltall("Lang dagpengeperiode", Mellomsteg, aldriSynlig)
+    private val terskelFaktor12 = Opplysningstype.somDesimaltall("Terskelfaktor for 12 måneder", Mellomsteg, aldriSynlig)
+    private val terskelFaktor36 = Opplysningstype.somDesimaltall("Terskelfaktor for 36 måneder", Mellomsteg, aldriSynlig)
+    private val divisor = Opplysningstype.somDesimaltall("Divisior", Mellomsteg, aldriSynlig)
+
     private val grunnbeløp = Minsteinntekt.grunnbeløp
-    private val terskel12 = Opplysningstype.somBeløp("Terskel for 12 måneder")
-    private val terskel36 = Opplysningstype.somBeløp("Terskel for 36 måneder")
+    private val terskel12 = Opplysningstype.somBeløp("Terskel for 12 måneder", Mellomsteg, aldriSynlig)
+    private val terskel36 = Opplysningstype.somBeløp("Terskel for 36 måneder", Mellomsteg, aldriSynlig)
+    private val inntektSnittSiste36 = Opplysningstype.somBeløp("Snittinntekt siste 36 måneder")
+
     private val inntektSiste12 = Minsteinntekt.inntekt12
     private val inntektSiste36 = Minsteinntekt.inntekt36
 
-    private val inntektSnittSiste36 = Opplysningstype.somBeløp("Snittinntekt siste 36 måneder")
-    private val divisor = Opplysningstype.somDesimaltall("Divisior")
+    private val stønadsuker12 = Opplysningstype.somHeltall("Stønadsuker ved siste 12 måneder", Mellomsteg, aldriSynlig)
+    private val stønadsuker36 = Opplysningstype.somHeltall("Stønadsuker ved siste 36 måneder", Mellomsteg, aldriSynlig)
 
-    private val langPeriode = Opplysningstype.somHeltall("Lang dagpengeperiode")
-    private val kortPeriode = Opplysningstype.somHeltall("Kort dagpengeperiode")
-    private val overterskel12 = Opplysningstype.somBoolsk("Over terskel for 12 måneder")
-    private val overterskel36 = Opplysningstype.somBoolsk("Over terskel for 36 måneder")
-    private val stønadsuker12 = Opplysningstype.somHeltall("Stønadsuker ved siste 12 måneder")
-    private val stønadsuker36 = Opplysningstype.somHeltall("Stønadsuker ved siste 36 måneder")
+    private val overterskel12 = Opplysningstype.somBoolsk("Over terskel for 12 måneder", Mellomsteg, aldriSynlig)
+    private val overterskel36 = Opplysningstype.somBoolsk("Over terskel for 36 måneder", Mellomsteg, aldriSynlig)
 
-    private val ingenOrdinærPeriode = Opplysningstype.somHeltall("Stønadsuker når kravet til minste arbeidsinntekt ikke er oppfylt")
+    private val antallStønadsuker = Opplysningstype.somHeltall("Antall stønadsuker", Mellomsteg, aldriSynlig)
+    private val gjenståendeStønadsdager = Opplysningstype.somHeltall("Antall gjenstående stønadsdager", Mellomsteg, aldriSynlig)
+
+    private val ingenOrdinærPeriode =
+        Opplysningstype.somHeltall("Stønadsuker når kravet til minste arbeidsinntekt ikke er oppfylt", Mellomsteg, aldriSynlig)
+
+    val ordinærPeriode = Opplysningstype.somHeltall("Antall stønadsuker som gis ved ordinære dagpenger")
 
     val regelsett =
         Regelsett(
